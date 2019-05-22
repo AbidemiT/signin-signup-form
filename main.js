c789cc1145961b4f4aa4dfd3d88d66e3adc70f2a
AbidemiT <abidemis01@gmail.com>
2019-05-22 14:15:13 +0100

    Added Sign In validation

--- main.js
+++ main.js
@@ -17,3 +17,41 @@ btnSignUp.addEventListener("click", () => {
     signIn.classList.toggle("block");
     signUp.classList.toggle("block");
 });
+
+/* 
+===================================
+    Form Validation starts here
+===================================
+*/
+
+let username = document.querySelector("#username");
+let usernamePassword = document.querySelector("#usernamePassword");
+let userSignIn = document.querySelector(".signIn");
+let usernameWarning = document.querySelector('.username-warning');
+let passwordWarning = document.querySelector('.password-warning');
+
+let userValidate = (e) => {
+    e.preventDefault();
+    if (username.value.length >= 1 ) {
+        if ( usernamePassword.value.length > 6 ) {
+            alert('Validation In progress...');
+        } else {
+            usernamePassword.style.border = "2px red solid";
+            passwordWarning.style.display = "block";
+            usernamePassword.addEventListener('keydown', () => {
+                usernamePassword.style.border = "none"; 
+                passwordWarning.style.display = "none";
+            }); 
+        }     
+    } else {
+        username.style.border = "2px red solid";
+        usernameWarning.style.display = "block";
+        username.addEventListener("keydown", () => {
+            username.style.border = "none";
+            usernameWarning.style.display = "none";
+        });
+    }
+     
+}
+
+userSignIn.addEventListener('click', userValidate);
